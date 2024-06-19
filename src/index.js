06ce549bc1ebab93e7f7c616b37505db946d66b5
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-06-19 21:47:30 +0200

    edit component pizza

--- src/index.js
+++ src/index.js
@@ -67,30 +67,27 @@ function Header() {
 
 function Menu() {
   return (
-    <div className="menu">
+    <ul className="menu">
       <h2>Our menu</h2>
       <div className="pizzas">
-        <Pizza
-          name="Pizza Spinaci"
-          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-          photoName="pizzas/spinaci.jpg"
-          price="10"
-        />
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
       </div>
-    </div>
+    </ul>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
